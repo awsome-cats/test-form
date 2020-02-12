22c5a2ef0bf48b100a67455e33e10c5baff90ef1
awesome-cats <cattwin82@gmail.com>
2020-02-13 08:46:53 +0900

    form changed

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,27 +1,35 @@
 <template>
-  <form @submit="sendForm" name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input v-model="form.name" type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input v-model="form.email" type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea v-model="form.message" name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
+  <form @submit.prevent="sendForm" name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+     <p class="hidden" style="display: none;">
+        <label>Don't fill this out if you're human:
+          <input name="bot-field"/>
+        </label>
+      </p>
+      <p>
+        <label>Your Name: <input v-model="form.name"     type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input v-model="form.email"      type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea v-model="form.message"    name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
 </form>
+<!------
+   netlify
+   data-netlify="true"
+   data-netlify-honeypot="bot-field"
+   name属性
+   formのmethod
+
+-------->
 </template>
 
 <script>
+import axios from 'axios'
 export default {
    data() {
       return {
@@ -39,17 +47,17 @@ export default {
          .join('&')
       },
       sendForm() {
-         fetch('/', {
-            method:'post',
-            headers:{
-               'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: this.encode({
-               'form-name': 'contact',
+         const axiosConfig = {
+            header:{
+               "Content-Type": "application/x-www-form-urlencoded"
+            }
+         };
+         axios.post("/", this.encode({
+               "form-name": "contact",
                ...this.form
-            })
-         })
-         .then(() => console.log('successfully'))
+            }), axiosConfig)
+         
+         .then(() => console.log("successfully"))
          .catch((e) => console.error(e))
       }
    }
