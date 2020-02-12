d5134820223342d8eacd0ea46e47de49012c6c5e
awesome-cats <cattwin82@gmail.com>
2020-02-13 06:19:05 +0900

    change form

---
+++ src/components/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input v-model="form.name" type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input v-model="form.email" type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea v-model="form.message" name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         form: {
+            name: '',
+            email: '',
+            message: ''
+         }
+      }
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
